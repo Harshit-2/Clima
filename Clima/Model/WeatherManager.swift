d8a4a0f727f20e1b8e1cf09f4cd7779751062764
Harshit <harshit242003@hotmail.com>
2024-01-05 00:24:39 +0530

    Complete Clima

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -15,7 +15,7 @@ protocol WeatherManagerDelegate {
 }
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=e72ca729af228beabd5d20e3b7749713&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid={apiKey}&units=metric"
     
     var delegate: WeatherManagerDelegate?
     
@@ -64,8 +64,4 @@ struct WeatherManager {
         }
     }
     
-    
-    
 }
-
-
